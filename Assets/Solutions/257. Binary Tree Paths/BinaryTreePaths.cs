932611279f3167710a5e13aa8d11e2204b731e33
Jin Chen <jin7157@yahoo.com.tw>
2022-09-30 23:49:05 +0800

    [chore] Binary Tree Paths with no recursive version.

--- Assets/Solutions/257. Binary Tree Paths/BinaryTreePaths.cs
+++ Assets/Solutions/257. Binary Tree Paths/BinaryTreePaths.cs
@@ -1,38 +1,58 @@
 using System.Collections.Generic;
+using System.Text;
 
 namespace BinaryTreePaths
 {
     public class Solution
     {
-        private const string PATH_FORMAT = "{0}->{1}";
+        private const int ZERO = 0;
+        private readonly StringBuilder stringBuilder = new StringBuilder();
+        private const string PATH_TO = "->";
 
         public IList<string> BinaryTreePaths(TreeNode root)
         {
             List<string> output = new List<string>();
+            Stack<(TreeNode, string)> callStack = new Stack<(TreeNode, string)>();
+            callStack.Push((root, root.val.ToString()));
 
-            BinaryTreePaths(output, root, root.val.ToString());
+            (TreeNode, string) _headTuple;
+            TreeNode _head;
+            TreeNode _pointer;
+            while (callStack.Count > ZERO)
+            {
+                _headTuple = callStack.Pop();
+                _head = _headTuple.Item1;
+
+                if (_head.left == null && _head.right == null)
+                {
+                    output.Add(_headTuple.Item2);
+                    continue;
+                }
+
+                _pointer = _head.right;
+                if (_pointer != null)
+                {
+                    callStack.Push((_pointer, ToPath(ref _headTuple.Item2, _pointer)));
+                }
+
+                _pointer = _head.left;
+                if (_pointer != null)
+                {
+                    callStack.Push((_pointer, ToPath(ref _headTuple.Item2, _pointer)));
+                }
+            }
 
             return output;
         }
 
-        private void BinaryTreePaths(List<string> output, TreeNode node, string s)
+        private string ToPath(ref string originalPath, TreeNode node)
         {
-            // A leaf is a node with no children
-            if (node.left == null && node.right == null)
-            {
-                output.Add(s);
-                return;
-            }
+            stringBuilder.Length = 0;
+            stringBuilder.Append(originalPath);
+            stringBuilder.Append(PATH_TO);
+            stringBuilder.Append(node.val);
 
-            if (node.left != null)
-            {
-                BinaryTreePaths(output, node.left, string.Format(PATH_FORMAT, s, node.left.val));
-            }
-
-            if (node.right != null)
-            {
-                BinaryTreePaths(output, node.right, string.Format(PATH_FORMAT, s, node.right.val));
-            }
+            return stringBuilder.ToString();
         }
     }
 }
